7ddd2a5b08098a3b1c3b231ddb04031875831de2
Greyson Filho <greysonmrx@gmail.com>
2021-04-21 21:11:56 -0300

    add movies

---
+++ movies/featuredMovies.js
@@ -0,0 +1,174 @@
+module.exports = [
+    {
+        id: 8,
+        title: 'Capitão América 2: O Soldado Invernal',
+        duration: '2h 16m',
+        synopsis: 'Após os eventos catastróficos em Nova York com Os Vingadores, Steve Rogers, também conhecido como Capitão América, segue tentando se ajustar ao mundo moderno. Porém, quando um colega da agência S.H.I.E.L.D. é atacado, Steve se vê preso em uma rede de intrigas que ameaça colocar o mundo em risco.',
+        rating: 5,
+        banner: './images/banners/captain_america_winter_soldier.jpg',
+        cover: './images/covers/captain_america_the_winter_soldier.jpg',
+        genre: 'Ação/Aventura',
+        releaseDate: 2014,
+        cast: [
+            {
+                id: 1,
+                name: 'Chris Evans',
+                character: 'Capitão América',
+                image: './images/actors/chris_evans.jpg'
+            },
+            {
+                id: 2,
+                name: 'Scarlett Johansson',
+                character: 'Viúva Negra',
+                image: './images/actors/scarlett_johansson.jpg'
+            },
+            {
+                id: 3,
+                name: 'Anthony Mackie',
+                character: 'Sam Wilson',
+                image: './images/actors/mackie_anthony.jpg'
+            },
+            {
+                id: 4,
+                name: 'Sebastian Stan',
+                character: 'Bucky Barnes',
+                image: './images/actors/stan_sebastian.jpg'
+            },
+            {
+                id: 5,
+                name: 'Emily VanCamp',
+                character: 'Sharon Carter',
+                image: './images/actors/vancamp_emily.jpg'
+            }
+        ]
+    },
+    {
+        id: 9,
+        title: 'The Avengers: Os Vingadores',
+        duration: '2h 24m',
+        synopsis: 'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes.',
+        rating: 5,
+        banner: './images/banners/avengers.jpg',
+        cover: './images/covers/the_avengers.jpg',
+        genre: 'Aventura/Ação',
+        releaseDate: 2012,
+        cast: [
+            {
+                id: 1,
+                name: 'Scarlett Johansson',
+                character: 'Viúva Negra',
+                image: './images/actors/scarlett_johansson.jpg'
+            },
+            {
+                id: 2,
+                name: 'Robert Downey Jr.',
+                character: 'Homem de Ferro',
+                image: './images/actors/robert_downey_jr.jpg'
+            },
+            {
+                id: 3,
+                name: 'Chris Evans',
+                character: 'Capitão América',
+                image: './images/actors/chris_evans.jpg'
+            },
+            {
+                id: 4,
+                name: 'Chris Hemsworth',
+                character: 'Thor',
+                image: './images/actors/hemsworth_chris.jpg'
+            },
+            {
+                id: 5,
+                name: 'Mark Ruffalo',
+                character: 'Hulk',
+                image: './images/actors/ruffalo_mark.jpg'
+            }
+        ]
+    },
+    {
+        id: 10,
+        title: 'Capitão América: Soldado Invernal',
+        duration: '3h 2m',
+        synopsis: 'Após Thanos eliminar metade das criaturas vivas, os Vingadores têm de lidar com a perda de amigos e entes queridos. Com Tony Stark vagando perdido no espaço sem água e comida, Steve Rogers e Natasha Romanov lideram a resistência contra o titã louco.',
+        rating: 4,
+        banner: './images/banners/endgame.jpg',
+        cover: './images/covers/avengers_endgame.jpg',
+        genre: 'Ação/Ficção científica',
+        releaseDate: 2019,
+        cast: [
+            {
+                id: 1,
+                name: 'Chris Evans',
+                character: 'Capitão América',
+                image: './images/actors/chris_evans.jpg'
+            },
+            {
+                id: 2,
+                name: 'Chris Hemsworth',
+                character: 'Thor',
+                image: './images/actors/hemsworth_chris.jpg'
+            },
+            {
+                id: 3,
+                name: 'Robert Downey Jr.',
+                character: 'Homem de Ferro',
+                image: './images/actors/robert_downey_jr.jpg'
+            },
+            {
+                id: 4,
+                name: 'Jeremy Renner',
+                character: 'Gavião Arqueiro',
+                image: './images/actors/renner_jeremy.jpg'
+            },
+            {
+                id: 5,
+                name: 'Mark Ruffalo',
+                character: 'Hulk',
+                image: './images/actors/ruffalo_mark.jpg'
+            }
+        ]
+    },
+    {
+        id: 11,
+        title: 'Homem de Ferro',
+        duration: '2h 6m',
+        synopsis: 'Tony Stark é um industrial bilionário e inventor brilhante que realiza testes bélicos no exterior, mas é sequestrado por terroristas que o forçam a construir uma arma devastadora. Em vez disso, ele constrói uma armadura blindada e enfrenta seus sequestradores. Ao voltar para os EUA, Stark aprimora a armadura e a utiliza para combater o crime.',
+        rating: 4,
+        banner: './images/banners/iron_man.jpg',
+        cover: './images/covers/iron_man.jpg',
+        genre: 'Ação/Aventura',
+        releaseDate: 2008,
+        cast: [
+            {
+                id: 1,
+                name: 'Robert Downey Jr.',
+                character: 'Homem de Ferro',
+                image: './images/actors/robert_downey_jr.jpg'
+            },
+            {
+                id: 2,
+                name: 'Jon Favreau',
+                character: 'Happy Hogan',
+                image: './images/actors/jon_favreau.jpg'
+            },
+            {
+                id: 3,
+                name: 'Gwyneth Paltrow',
+                character: 'Pepper Potts',
+                image: './images/actors/paltrow_gwyneth.jpg'
+            },
+            {
+                id: 4,
+                name: 'Faran Tahir',
+                character: 'Raza',
+                image: './images/actors/faran_tahir.jpg'
+            },
+            {
+                id: 5,
+                name: 'Jeff Bridges',
+                character: 'Obadiah Stane',
+                image: './images/actors/jeffrey_bridges.jpg'
+            },
+        ]
+    },
+];
---
+++ movies/movies.js
@@ -0,0 +1,392 @@
+module.exports = [
+    {
+        id: 1,
+        title: 'Vingadores: Era de Ultron',
+        duration: '2h 22m',
+        synopsis: 'Ao tentar proteger o planeta de ameaças, Tony Stark constrói um sistema de inteligência artificial que cuidaria da paz mundial. O projeto acaba dando errado e gera o nascimento do Ultron. Com o destino da Terra em jogo, Capitão América, Homem de Ferro, Thor, Hulk, Viúva Negra e Gavião Arqueiro terão que se unir para mais uma vez salvar a raça humana da extinção.',
+        rating: 4,
+        cover: './images/covers/avengers_age_of_ultron.jpg',
+        genre: 'Ação/Aventura',
+        releaseDate: 2015,
+        cast: [
+            {
+                id: 1,
+                name: 'Scarlett Johansson',
+                character: 'Viúva Negra',
+                image: './images/actors/scarlett_johansson.jpg'
+            },
+            {
+                id: 2,
+                name: 'Robert Downey Jr.',
+                character: 'Homem de Ferro',
+                image: './images/actors/robert_downey_jr.jpg'
+            },
+            {
+                id: 3,
+                name: 'Chris Evans',
+                character: 'Capitão América',
+                image: './images/actors/chris_evans.jpg'
+            },
+            {
+                id: 4,
+                name: 'Elizabeth Olsen',
+                character: 'Feiticeira Escarlate',
+                image: './images/actors/elizabeth_olsen.jpg'
+            },
+            {
+                id: 5,
+                name: 'James Spader',
+                character: 'Ultron',
+                image: './images/actors/james_spader.jpg'
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'Capitão América: Guerra Civil',
+        duration: '2h 28m',
+        synopsis: 'Depois do ataque de Ultron, os políticos decidem controlar os Vingadores, já que suas ações afetam toda a humanidade. A decisão coloca o Capitão América em rota de colisão com o Homem de Ferro.',
+        rating: 5,
+        cover: './images/covers/captain_america_civil_war.jpg',
+        genre: 'Ação/Aventura',
+        releaseDate: 2016,
+        cast: [
+            {
+                id: 1,
+                name: 'Sebastian Stan',
+                character: 'Bucky Barnes',
+                image: './images/actors/stan_sebastian.jpg'
+            },
+            {
+                id: 2,
+                name: 'Robert Downey Jr.',
+                character: 'Homem de Ferro',
+                image: './images/actors/robert_downey_jr.jpg'
+            },
+            {
+                id: 3,
+                name: 'Chris Evans',
+                character: 'Capitão América',
+                image: './images/actors/chris_evans.jpg'
+            },
+            {
+                id: 4,
+                name: 'Elizabeth Olsen',
+                character: 'Feiticeira Escarlate',
+                image: './images/actors/elizabeth_olsen.jpg'
+            },
+            {
+                id: 5,
+                name: 'Anthony Mackie',
+                character: 'Sam Wilson',
+                image: './images/actors/mackie_anthony.jpg'
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: 'Capitão América: O Primeiro Vingador',
+        duration: '2h 4m',
+        synopsis: 'Steve Rogers é um jovem que participa de experiências visando a criação do supersoldado americano. Quando os oficiais militares conseguem transformá-lo em uma arma humana, eles percebem que não podem arriscar a vida do jovem nas batalhas de guerra.',
+        rating: 5,
+        cover: './images/covers/captain_america_the_first_avenger.jpg',
+        genre: 'Ação/Aventura',
+        releaseDate: 2011,
+        cast: [
+            {
+                id: 1,
+                name: 'Sebastian Stan',
+                character: 'Bucky Barnes',
+                image: './images/actors/stan_sebastian.jpg'
+            },
+            {
+                id: 2,
+                name: 'Hayley Atwell',
+                character: 'Peggy Carter',
+                image: './images/actors/hayley_atwell.jpg'
+            },
+            {
+                id: 3,
+                name: 'Chris Evans',
+                character: 'Capitão América',
+                image: './images/actors/chris_evans.jpg'
+            },
+        ]
+    },
+    {
+        id: 4,
+        title: 'Deadpool',
+        duration: '1h 49m',
+        synopsis: 'Wade Wilson é um ex-agente especial que passou a trabalhar como mercenário. Seu mundo é destruído quando um cientista maligno o tortura e o desfigura completamente. O experimento brutal transforma Wade em Deadpool, que ganha poderes especiais de cura e uma força aprimorada.',
+        rating: 3,
+        cover: './images/covers/deadpool.jpg',
+        genre: 'Ação/Comédia',
+        releaseDate: 2016,
+        cast: [
+            {
+                id: 1,
+                name: 'Ryan Reynolds',
+                character: 'Deadpool',
+                image: './images/actors/reynolds_ryan.jpg'
+            },
+            {
+                id: 2,
+                name: 'Morena Baccarin',
+                character: 'Vanessa',
+                image: './images/actors/baccarin_morena.jpg'
+            },
+        ]
+    },
+    {
+        id: 5,
+        title: 'Homem de Ferro 3',
+        duration: '2h 11m',
+        synopsis: 'Depois de um inimigo reduzir o mundo de Tony Stark a destroços, o Homem de Ferro precisa aprender a confiar em seus instintos para proteger aqueles que ama, especialmente sua namorada, e lutar contra seu maior medo: o fracasso.',
+        rating: 2,
+        cover: './images/covers/iron_man_3.jpg',
+        genre: 'Ação/Ficção científica',
+        releaseDate: 2013,
+        cast: [
+            {
+                id: 1,
+                name: 'Robert Downey Jr.',
+                character: 'Homem de Ferro',
+                image: './images/actors/robert_downey_jr.jpg'
+            },
+            {
+                id: 2,
+                name: 'Gwyneth Paltrow',
+                character: 'Pepper Potts',
+                image: './images/actors/paltrow_gwyneth.jpg'
+            },
+        ]
+    },
+    {
+        id: 6,
+        title: 'Coringa',
+        duration: '2h 2m',
+        synopsis: 'Isolado, intimidado e desconsiderado pela sociedade, o fracassado comediante Arthur Fleck inicia seu caminho como uma mente criminosa após assassinar três homens em pleno metrô. Sua ação inicia um movimento popular contra a elite de Gotham City, da qual Thomas Wayne é seu maior representante.',
+        rating: 2,
+        cover: './images/covers/iron_man_3.jpg',
+        genre: 'Crime/Drama',
+        releaseDate: 2019,
+        cast: [
+            {
+                id: 1,
+                name: 'Joaquin Phoenix',
+                character: 'Coringa',
+                image: './images/actors/phoenix_joaquin.jpg'
+            },
+            {
+                id: 2,
+                name: 'Robert De Niro',
+                character: 'Murray Franklin',
+                image: './images/actors/deniro_r.jpg'
+            },
+        ]
+    },
+    {
+        id: 7,
+        title: 'O Espetacular Homem-Aranha',
+        duration: '2h 33m',
+        synopsis: 'Peter Parker está tentando saber mais sobre sua origem. Ele encontra uma pasta que pertenceu ao seu pai e quer descobrir por que seus pais desapareceram. Sua busca o leva a Oscorp e ao dr. Curt Connors, que tem como alterego o letal Lagarto.',
+        rating: 4,
+        cover: './images/covers/iron_man_3.jpg',
+        genre: 'Ação/Aventura',
+        releaseDate: 2012,
+        cast: [
+            {
+                id: 1,
+                name: 'Andrew Garfield',
+                character: 'Homem-Aranha',
+                image: './images/actors/garfield_andrew.jpg'
+            },
+            {
+                id: 2,
+                name: 'Emma Stone',
+                character: 'Gwen Stacy',
+                image: './images/actors/stone_emma.jpg'
+            },
+            {
+                id: 3,
+                name: 'Stan Lee',
+                character: 'Bibliotecário da escola',
+                image: './images/actors/lee_stan.jpg'
+            },
+            {
+                id: 4,
+                name: 'Sally Field',
+                character: 'May Parker',
+                image: './images/actors/field_sally.jpg'
+            }
+        ]
+    },
+    {
+        id: 8,
+        title: 'Capitão América 2: O Soldado Invernal',
+        duration: '2h 16m',
+        synopsis: 'Após os eventos catastróficos em Nova York com Os Vingadores, Steve Rogers, também conhecido como Capitão América, segue tentando se ajustar ao mundo moderno. Porém, quando um colega da agência S.H.I.E.L.D. é atacado, Steve se vê preso em uma rede de intrigas que ameaça colocar o mundo em risco.',
+        rating: 5,
+        cover: './images/covers/captain_america_the_winter_soldier.jpg',
+        genre: 'Ação/Aventura',
+        releaseDate: 2014,
+        cast: [
+            {
+                id: 1,
+                name: 'Chris Evans',
+                character: 'Capitão América',
+                image: './images/actors/chris_evans.jpg'
+            },
+            {
+                id: 2,
+                name: 'Scarlett Johansson',
+                character: 'Viúva Negra',
+                image: './images/actors/scarlett_johansson.jpg'
+            },
+            {
+                id: 3,
+                name: 'Anthony Mackie',
+                character: 'Sam Wilson',
+                image: './images/actors/mackie_anthony.jpg'
+            },
+            {
+                id: 4,
+                name: 'Sebastian Stan',
+                character: 'Bucky Barnes',
+                image: './images/actors/stan_sebastian.jpg'
+            },
+            {
+                id: 5,
+                name: 'Emily VanCamp',
+                character: 'Sharon Carter',
+                image: './images/actors/vancamp_emily.jpg'
+            }
+        ]
+    },
+    {
+        id: 9,
+        title: 'The Avengers: Os Vingadores',
+        duration: '2h 24m',
+        synopsis: 'Loki, o irmão de Thor, ganha acesso ao poder ilimitado do cubo cósmico ao roubá-lo de dentro das instalações da S.H.I.E.L.D. Nick Fury, o diretor desta agência internacional que mantém a paz, logo reúne os únicos super-heróis que serão capazes de defender a Terra de ameaças sem precedentes.',
+        rating: 5,
+        cover: './images/covers/the_avengers.jpg',
+        genre: 'Aventura/Ação',
+        releaseDate: 2012,
+        cast: [
+            {
+                id: 1,
+                name: 'Scarlett Johansson',
+                character: 'Viúva Negra',
+                image: './images/actors/scarlett_johansson.jpg'
+            },
+            {
+                id: 2,
+                name: 'Robert Downey Jr.',
+                character: 'Homem de Ferro',
+                image: './images/actors/robert_downey_jr.jpg'
+            },
+            {
+                id: 3,
+                name: 'Chris Evans',
+                character: 'Capitão América',
+                image: './images/actors/chris_evans.jpg'
+            },
+            {
+                id: 4,
+                name: 'Chris Hemsworth',
+                character: 'Thor',
+                image: './images/actors/hemsworth_chris.jpg'
+            },
+            {
+                id: 5,
+                name: 'Mark Ruffalo',
+                character: 'Hulk',
+                image: './images/actors/ruffalo_mark.jpg'
+            }
+        ]
+    },
+    {
+        id: 10,
+        title: 'Capitão América: Soldado Invernal',
+        duration: '3h 2m',
+        synopsis: 'Após Thanos eliminar metade das criaturas vivas, os Vingadores têm de lidar com a perda de amigos e entes queridos. Com Tony Stark vagando perdido no espaço sem água e comida, Steve Rogers e Natasha Romanov lideram a resistência contra o titã louco.',
+        rating: 4,
+        cover: './images/covers/avengers_endgame.jpg',
+        genre: 'Ação/Ficção científica',
+        releaseDate: 2019,
+        cast: [
+            {
+                id: 1,
+                name: 'Chris Evans',
+                character: 'Capitão América',
+                image: './images/actors/chris_evans.jpg'
+            },
+            {
+                id: 2,
+                name: 'Chris Hemsworth',
+                character: 'Thor',
+                image: './images/actors/hemsworth_chris.jpg'
+            },
+            {
+                id: 3,
+                name: 'Robert Downey Jr.',
+                character: 'Homem de Ferro',
+                image: './images/actors/robert_downey_jr.jpg'
+            },
+            {
+                id: 4,
+                name: 'Jeremy Renner',
+                character: 'Gavião Arqueiro',
+                image: './images/actors/renner_jeremy.jpg'
+            },
+            {
+                id: 5,
+                name: 'Mark Ruffalo',
+                character: 'Hulk',
+                image: './images/actors/ruffalo_mark.jpg'
+            }
+        ]
+    },
+    {
+        id: 11,
+        title: 'Homem de Ferro',
+        duration: '2h 6m',
+        synopsis: 'Tony Stark é um industrial bilionário e inventor brilhante que realiza testes bélicos no exterior, mas é sequestrado por terroristas que o forçam a construir uma arma devastadora. Em vez disso, ele constrói uma armadura blindada e enfrenta seus sequestradores. Ao voltar para os EUA, Stark aprimora a armadura e a utiliza para combater o crime.',
+        rating: 4,
+        cover: './images/covers/iron_man.jpg',
+        genre: 'Ação/Aventura',
+        releaseDate: 2008,
+        cast: [
+            {
+                id: 1,
+                name: 'Robert Downey Jr.',
+                character: 'Homem de Ferro',
+                image: './images/actors/robert_downey_jr.jpg'
+            },
+            {
+                id: 2,
+                name: 'Jon Favreau',
+                character: 'Happy Hogan',
+                image: './images/actors/jon_favreau.jpg'
+            },
+            {
+                id: 3,
+                name: 'Gwyneth Paltrow',
+                character: 'Pepper Potts',
+                image: './images/actors/paltrow_gwyneth.jpg'
+            },
+            {
+                id: 4,
+                name: 'Faran Tahir',
+                character: 'Raza',
+                image: './images/actors/faran_tahir.jpg'
+            },
+            {
+                id: 5,
+                name: 'Jeff Bridges',
+                character: 'Obadiah Stane',
+                image: './images/actors/jeffrey_bridges.jpg'
+            },
+        ]
+    },
+];
