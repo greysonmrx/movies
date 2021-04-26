5e68977433eec5923ab11e3a71ed7806a02b4553
Greyson Filho <greysonmrx@gmail.com>
2021-04-25 21:00:37 -0300

    video 3

--- MovieApp/App.js
+++ MovieApp/App.js
@@ -1,14 +1,8 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { 
-  View, 
-  Text, 
-  Image, 
-  StyleSheet, 
-  FlatList,
-  ScrollView,
-  TouchableOpacity,
-  Dimensions
-} from 'react-native';
+import { View, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 import {
   useFonts,
   Poppins_400Regular,
@@ -16,17 +10,12 @@ import {
   Poppins_600SemiBold,
   Poppins_800ExtraBold
 } from '@expo-google-fonts/poppins';
-import { StatusBar } from 'expo-status-bar';
-import Constants from 'expo-constants';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
-import MovieItem from './src/components/MovieItem';
+import Home from './src/pages/Home';
 
-import profileImg from './src/assets/profile.jpg';
-
-import featuredMovies from './src/assets/featuredMovies';
-import movies from './src/assets/movies';
-
-const WIDTH = Dimensions.get('window').width;
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -45,95 +34,44 @@ export default function App() {
   }
 
   return (
-    <ScrollView 
-      style={styles.container} 
-      contentContainerStyle={{ paddingTop: 20 }}
-    >
-      <StatusBar style="light"/>
-      <View style={styles.header}>
-        <View>
-          <Text style={styles.message}>Olá,</Text>
-          <Text style={styles.name}>Greyson Santos</Text>
-        </View>
-        <Image source={profileImg} style={styles.image}/>
-      </View>
-      <FlatList
-        data={featuredMovies}
-        contentContainerStyle={{ paddingHorizontal: 20 }}
-        keyExtractor={(item) => item.id.toString()}
-        horizontal={true}
-        pagingEnabled={true}
-        snapToInterval={WIDTH - 20}
-        ItemSeparatorComponent={() => <View style={{ width: 20 }}/>}
-        renderItem={({ item }) => (
-          <TouchableOpacity style={styles.bannerButton}>
-            <Image source={{ uri: item.banner }} style={styles.bannerImage}/>
-          </TouchableOpacity>
-        )}
-      />
-      <View style={styles.moviesList}>
-          <Text style={styles.moviesListTitle}>Lançamentos</Text>
-          {
-            movies.map((item) => (
-              <MovieItem 
-                key={item.id.toString()}
-                cover={item.cover}
-                title={item.title}
-                synopsis={item.synopsis}
-                rating={item.rating}
-              />
-            ))
-          }
-      </View>
-    </ScrollView>
+    <NavigationContainer>
+      <Tab.Navigator
+        barStyle={{ backgroundColor: '#121A23' }}
+        activeColor="#4B97C5"
+        inactiveColor="#3C434A"
+        shifting={true}
+      >
+        <Tab.Screen 
+          name="Home" 
+          component={Home}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Feather name="home" size={24} color={color}/>
+            ),
+            title: 'Início',
+          }}
+        />
+        <Tab.Screen 
+          name="Search" 
+          component={Home}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Feather name="search" size={24} color={color}/>
+            ),
+            title: 'Busca'
+          }}
+        />
+        <Tab.Screen 
+          name="Favorites" 
+          component={Home}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <Feather name="bookmark" size={24} color={color}/>
+            ),
+            title: 'Favoritos'
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#111820'
-  },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 20,
-    paddingBottom: 35,
-    paddingTop: Constants.statusBarHeight
-  },
-  message: {
-    color: '#64696E',
-    fontSize: 18,
-    fontFamily: 'Poppins_400Regular'
-  },
-  name: {
-    color: '#FFFFFF',
-    fontSize: 20,
-    lineHeight: 24,
-    fontFamily: 'Poppins_600SemiBold'
-  },
-  image: {
-    width: 40,
-    height: 40,
-    borderRadius: 20
-  },
-  bannerButton:{
-    width: WIDTH - 40,
-    aspectRatio: 1.78
-  },
-  bannerImage: {
-    height: '100%',
-    width: '100%',
-    borderRadius: 5
-  },
-  moviesList: {
-    padding: 20
-  },
-  moviesListTitle: {
-    color: '#FFFFFF',
-    fontFamily: 'Poppins_600SemiBold',
-    fontSize: 18,
-    marginBottom: 15
-  },
-});
---
+++ MovieApp/src/pages/Home.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import { 
+    ScrollView,
+    View, 
+    Text, 
+    Image, 
+    FlatList,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions
+} from 'react-native';
+import Constants from 'expo-constants';
+
+import MovieItem from '../components/MovieItem';
+
+import profileImg from '../assets/profile.jpg';
+
+import featuredMovies from '../assets/featuredMovies';
+import movies from '../assets/movies';
+
+const WIDTH = Dimensions.get('window').width;
+
+export default function Home() {
+    return (
+        <ScrollView 
+            style={styles.container} 
+            contentContainerStyle={{ paddingTop: 20 }}
+        >
+            <View style={styles.header}>
+                <View>
+                <Text style={styles.message}>Olá,</Text>
+                <Text style={styles.name}>Greyson Santos</Text>
+                </View>
+                <Image source={profileImg} style={styles.image}/>
+            </View>
+            <FlatList
+                data={featuredMovies}
+                contentContainerStyle={{ paddingHorizontal: 20 }}
+                keyExtractor={(item) => item.id.toString()}
+                horizontal={true}
+                pagingEnabled={true}
+                snapToInterval={WIDTH - 20}
+                ItemSeparatorComponent={() => <View style={{ width: 20 }}/>}
+                renderItem={({ item }) => (
+                <TouchableOpacity style={styles.bannerButton}>
+                    <Image source={{ uri: item.banner }} style={styles.bannerImage}/>
+                </TouchableOpacity>
+                )}
+            />
+            <View style={styles.moviesList}>
+                <Text style={styles.moviesListTitle}>Lançamentos</Text>
+                {
+                    movies.map((item) => (
+                    <MovieItem 
+                        key={item.id.toString()}
+                        cover={item.cover}
+                        title={item.title}
+                        synopsis={item.synopsis}
+                        rating={item.rating}
+                    />
+                    ))
+                }
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#111820'
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 20,
+        paddingBottom: 35,
+        paddingTop: Constants.statusBarHeight
+    },
+    message: {
+        color: '#64696E',
+        fontSize: 18,
+        fontFamily: 'Poppins_400Regular'
+    },
+    name: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        lineHeight: 24,
+        fontFamily: 'Poppins_600SemiBold'
+    },
+    image: {
+        width: 40,
+        height: 40,
+        borderRadius: 20
+    },
+    bannerButton:{
+        width: WIDTH - 40,
+        aspectRatio: 1.78
+    },
+    bannerImage: {
+        height: '100%',
+        width: '100%',
+        borderRadius: 5
+    },
+    moviesList: {
+        padding: 20
+      },
+    moviesListTitle: {
+        color: '#FFFFFF',
+        fontFamily: 'Poppins_600SemiBold',
+        fontSize: 18,
+        marginBottom: 15
+    },
+});
